4001783443ee3901ace451bc31694a9befb691e0
Olinizm <a.cybulka.05@gmail.com>
2023-03-25 20:28:38 +0100

    OMG I finally made clicking work with multiple circles

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
     var mouseX = 0;
     var mouseY = 70;
+    var nrId = 0;
+    var cId = 0;
+    var cPosX = 0;
+    var cPosY = 0;
 
     var gameWindow = document.getElementById("game");
     var loffset = gameWindow.offsetLeft;
     var toffset = gameWindow.offsetTop;
-    var windowWid = gameWindow.offsetWidth
+    var windowWid = gameWindow.offsetWidth;
     
     $("#game").mousemove(function(event) {
         mouseX = event.pageX;
@@ -15,25 +19,52 @@ $(document).ready(function() {
                     .css("left", mouseX - 10);
     })
 
+    $(document).click(function() {
+        if(parseInt(Math.abs(mouseX - cPosX)) < 30 && parseInt(Math.abs(mouseY - cPosY)) < 30)
+        {
+            console.log("jest");
+            spawnCircle();
+            console.log(cId);
+            $("#"+cId).remove();
+            $("#"+cId+"o").remove();
+            cId++;
+            setCurrent(cId);
+        }
+    })
+
     function spawnCircle()
     {
         var inner = document.createElement('div');
-        inner.setAttribute("class", "inner_circle");
+        inner.setAttribute('class', "inner_circle");
+        inner.className = "inner_circle";
+        inner.setAttribute("id", nrId);
         var outer = document.createElement('div');
-        outer.setAttribute("class", "outer_circle");
+        outer.setAttribute('class', "outer_circle");
+        outer.setAttribute("id", nrId+"o");
         setPosition(inner, outer)
+        nrId++;
+        
         $("#game").prepend(inner);
         $("#game").prepend(outer);
     }
     function setPosition(inner, outer)
     {
         spawnPosX = Math.random()* (windowWid-70) + loffset;
-        spawnPosY = Math.random()* 550 + toffset;
+        spawnPosY = Math.random()* 530 + toffset;
         $(inner).css("left", spawnPosX+20)
                 .css("top", spawnPosY+20)
                           
         $(outer).css("left", spawnPosX)
                 .css("top", spawnPosY)
     }
+    function setCurrent(next)
+    {
+        var circle = document.getElementById(next);
+        cPosX = circle.offsetLeft+30;
+        cPosY = circle.offsetTop+30;
+        console.log(cPosX + " " + cPosY)
+        
+    }
     spawnCircle();
+    setCurrent(0);
 })
