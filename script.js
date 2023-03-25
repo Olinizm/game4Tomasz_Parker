3ee80d597d0f118e8db446d908eed1025e6cc1b8
Olinizm <a.cybulka.05@gmail.com>
2023-03-25 11:57:00 +0100

    basic game layout

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+    var mouseX = 0;
+    var mouseY = 70;
     
+    $("#game").mousemove(function(event) {
+        mouseX = event.pageX;
+        mouseY = event.pageY;
+        if(event.pageY < 70) mouseY = 70;
+        $("#cursor").css("top", mouseY - 10)
+                    .css("left", mouseX - 10);
+    })
+
+    function spawnCircle()
+    {
+        $(".inner_circle")
+    }
 })
